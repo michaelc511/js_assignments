0795a24dff00068159985f4efbef560a5f8e8db2
michaelc511 <michaelc511@gmail.com>
2024-01-08 09:10:14 -0800

    script 4

---
+++ ih/script4.js
@@ -0,0 +1,8 @@
+// Functions in JS
+// Top 100 JS Questions 3 hours: https://youtu.be/AUTO7ALJk2U?si=5U-W4-Jq034x2BnK
+
+
+/// Chap 6 Functions //////////////////////////////////////////////
+
+// 31
+
